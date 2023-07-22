1c9c452e2bf1f472ec6577211a71d72239d5f3fa
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-22 05:40:51 -0700

    updated main.c

--- main.c
+++ main.c
@@ -1,72 +1,62 @@
 #include "shell.h"
 /**
  * main - entry of the code
- * @ac: argument count
- * @argv: argument vector
  * Return: always 0
  */
-int main(int ac, char **av)
+int main(void)
 {
-	char *prompt = "jash@$> ";
-	char *line = NULL;
-	char *line_copy = NULL;
-	size_t i = 0;
-	ssize_t readc;
-	const char *delim = " \n";
-	int tokens_num = 0;
-	char *token;
-
-	(void)ac;
+	char **args, *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t characters_read;
+	char *command;
+	pid_t child_pid;
+	int status;
 
 	while (1)
 	{
-	printf("%s", prompt);
-	readc = getline(&line, &i, stdin);
-
-	if (readc == -1)
-	{
-		printf("Exiting jash ....\n");
-		return (-1);
+		printf("#cisfun$ ");
+		characters_read = getline(&buffer, &bufsize, stdin);
+		if (characters_read == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+		}
+		buffer[strcspn(buffer, "\n")] = '\0';
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			command = strtok(buffer, " ");
+			args = (char **)malloc(2 * sizeof(char *));
+			if (!args)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+			args[0] = command;
+			args[1] = NULL;
+			execve(command, args, NULL);
+			perror(command);
+			free(args);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+		}
 	}
-
-	line_copy = malloc(sizeof(char) * readc);
-	if (line_copy == NULL)
-	{
-		perror("Oops: memory allocation error");
-		return (-1);
-	}
-	strcpy(line_copy, line);
-
-	token = strtok(line, delim);
-	while (token != NULL)
-	{
-		tokens_num++;
-		token = strtok(NULL, delim);
-	}
-	tokens_num++;
-
-	av = malloc(sizeof(char *) * tokens_num);
-	token = strtok(line_copy, delim);
-	for (i = 0; token != NULL; i++)
-	{
-		av[i] = malloc(sizeof(char) * strlen(token));
-		strcpy(av[i], token);
-
-		token = strtok(NULL, delim);
-	}
-
-	av[i] = NULL;
-
-	if (strcmp(av[0], "exit") == 0)
-	{
-		exit_shell();
-		break;
-	}
-
-	execute_cmd(av);
-	}
-
-	free(line);
-	free(line_copy);
+	free(buffer);
 	return (0);
 }
