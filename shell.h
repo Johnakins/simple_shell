7ddc118d096edf8459aabf21e45f92ca5cee28c1
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-26 08:47:32 -0700

    headerfile

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,5 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #define BUFFER_SIZE 1024
-void run_shell(void);
+void execute_command(char *const args[]);
+char *read_input();
+char **tokenize_input(char *input);
+extern char **environ;
 #endif
