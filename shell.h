8a5e14a4eb8b43e2ee58d18805705dda73613611
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-27 01:48:46 -0700

    changed prototype in shell.h

--- shell.h
+++ shell.h
@@ -7,8 +7,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#define BUFFER_SIZE 1024
-void execute_command(char *const args[]);
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 64
+
+void execute_command(char* command);
 char *read_input();
 char **tokenize_input(char *input);
 void print_environment(void);
